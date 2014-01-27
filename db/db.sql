eb1f1496f41956a36d40693ddff76fac47144017
pepitweet <jrobertotrejo@hotmail.com>
2014-01-26 22:06:40 -0600

    Update DB

--- db/db.sql
+++ db/db.sql
@@ -6,46 +6,63 @@ use SICEN;
 /*
 	Se crean las tablas principales de la base de datos
 */
-
+/*
+	Catálogos del sistema
+*/
+create table if not exists cat_departamentos
+(
+	clave varchar(5) primary key,
+	departamento varchar(100) not null,
+	descripcion varchar(500),
+	activo bool default true
+);
+create table if not exists cat_puestos
+(
+	clave varchar(5) primary key,
+	claveDep varchar(5) references cat_departamentos(clave),
+	puesto varchar(100) not null,
+	descripcion varchar(500),
+	activo bool default true
+);
 
 /*
 	Catálogos complementon nomina
 */
 create table if not exists cat_RCT
 (
-	clave int primary key,
+	clave varchar(5) primary key,
 	descripcion varchar(200) not null,
 	activo bool default true
 );
 create table if not exists cat_Bancos
 (
-	clave int primary key,
+	clave varchar(5) primary key,
 	nombreCorto varchar(50) not null,
 	razonSocial varchar(300) not null,
 	activo bool default true
 );
 create table if not exists cat_RiesgoPuesto
 (
-	clave int primary key,
+	clave varchar(5) primary key,
 	Descripción varchar(100) not null,
 	activo bool default true
 );
 create table if not exists cat_TipoPercepcion
 (
-	clave int primary key,
+	clave varchar(5) primary key,
 	descripcion varchar(100) not null,
 	activo bool default true
 );
 create table if not exists cat_TipoDeduccion
 (
-	clave int primary key,
+	clave varchar(5) primary key,
 	descripcion varchar(200),
 	comentario varchar(200),
 	activo bool default true
 );
 create table if not exists cat_TipoIncapacidad
 (
-	clave int primary key,
+	clave varchar(5) primary key,
 	descripcion varchar(50),
-	activo bool defaul true
+	activo bool default true
 );
